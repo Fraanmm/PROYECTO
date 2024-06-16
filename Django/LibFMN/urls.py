eb7ad1787ae48618333531261d2ed327da05381b
fraanmm <fr.ortegau@duocuc.cl>
2024-06-16 16:57:07 -0400

    ulsy

--- Django/LibFMN/urls.py
+++ Django/LibFMN/urls.py
@@ -2,5 +2,26 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    
+   path("",views.index, name="index"),
+   path("",views.autorMes, name="autorMes"),
+   path("",views.cambioclimatico, name="cambioclimatico"),
+   path("",views.comicmanga, name="comicmanga"),
+   path("",views.descuentos, name="descuentos"),
+   path("",views.destacado, name="destacado"),
+   path("",views.harryPotter, name="harryPotter"),
+   path("",views.index2, name="index2"),
+   path("",views.literatura, name="literatura"),
+   path("",views.novelaholocausto, name="novelaholocausto"),
+   path("",views.promocionesEscolares, name="promocionesEscolares"),
+   path("",views.tematicasaga, name="tematicasaga"),
+   path("",views.diseño, name="diseño"),
+   path("",views.envio, name="envio"),
+   path("",views.tematicas, name="tematicas"),
+   path("",views.imgn, name="imgn"),
+   path("",views.dsdo, name="dsdo"),
+   path("",views.imgAutorMes, name="imgAutorMes"),
+   path("",views.app, name="app")
+
+
+
 ]
